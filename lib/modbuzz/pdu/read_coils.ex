e202676b2d74910ff6546429d67dcc6851adc761
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-08-12 20:14:25 +0900

    Add decode functions for error to read_coils.ex and write_single_coil.ex

--- lib/modbuzz/pdu/read_coils.ex
+++ lib/modbuzz/pdu/read_coils.ex
@@ -6,6 +6,7 @@ defimpl Modbuzz.PDU, for: Modbuzz.PDU.ReadCoils do
   import Bitwise
 
   @read_coils 0x01
+  @error_code @read_coils + 0x80
 
   def encode(struct) do
     <<@read_coils::8, struct.starting_address::16, struct.quantity_of_coils::16>>
@@ -17,5 +18,9 @@ defimpl Modbuzz.PDU, for: Modbuzz.PDU.ReadCoils do
     |> Enum.flat_map(&for(i <- 0..7, do: (&1 >>> i &&& 1) == 1))
     |> Enum.take(struct.quantity_of_coils)
     |> then(&{:ok, &1})
+  end
+
+  def decode(_struct, <<@error_code::8, exception_code::8>>) do
+    {:error, exception_code: exception_code}
   end
 end
--- lib/modbuzz/pdu/write_single_coil.ex
+++ lib/modbuzz/pdu/write_single_coil.ex
@@ -4,6 +4,7 @@ end
 
 defimpl Modbuzz.PDU, for: Modbuzz.PDU.WriteSingleCoil do
   @write_single_coil 0x05
+  @error_code @write_single_coil + 0x80
 
   def encode(struct) do
     output_value = if struct.output_value, do: 0xFF00, else: 0x0000
@@ -12,5 +13,9 @@ defimpl Modbuzz.PDU, for: Modbuzz.PDU.WriteSingleCoil do
 
   def decode(_struct, <<@write_single_coil::8, _output_address::16, _output_value::16>>) do
     :ok
+  end
+
+  def decode(_struct, <<@error_code::8, exception_code::8>>) do
+    {:error, exception_code: exception_code}
   end
 end

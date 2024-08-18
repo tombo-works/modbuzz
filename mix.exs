374c159bb9b45685274f3daa765ecfcbb376659c
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-08-18 17:58:29 +0900

    Add credo to deps

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Modbuzz.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 end

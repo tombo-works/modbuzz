43239509a382b3a5d85c56d71bb01a300faa9c70
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-08-22 14:43:51 +0900

    Refactor

--- lib/modbuzz/pdu/helper.ex
+++ lib/modbuzz/pdu/helper.ex
@@ -3,16 +3,16 @@ defmodule Modbuzz.PDU.Helper do
 
   import Bitwise
 
+  def module_one_line_doc(module) when is_atom(module) do
+    type = module |> Module.split() |> List.last()
+    "`Modbuzz.PDU` implementation of #{type}."
+  end
+
   @spec to_booleans(binary()) :: [] | [boolean()]
   def to_booleans(binary) when is_binary(binary) do
     binary
     |> :binary.bin_to_list()
     |> Enum.flat_map(&for(i <- 0..7, do: (&1 >>> i &&& 1) == 1))
-  end
-
-  def module_one_line_doc(module) when is_atom(module) do
-    type = module |> Module.split() |> List.last()
-    "`Modbuzz.PDU` implementation of #{type}."
   end
 
   @spec to_registers(binary()) :: [] | [non_neg_integer()]

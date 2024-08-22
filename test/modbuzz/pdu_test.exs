70ea106fc8f633d0dbd836267c5b7809d845aa50
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-08-22 15:31:21 +0900

    Add ReadInputRegisters

--- test/modbuzz/pdu_test.exs
+++ test/modbuzz/pdu_test.exs
@@ -4,5 +4,6 @@ defmodule Modbuzz.PDUTest do
   doctest Modbuzz.PDU.Modbuzz.PDU.ReadCoils
   doctest Modbuzz.PDU.Modbuzz.PDU.ReadDiscreteInputs
   doctest Modbuzz.PDU.Modbuzz.PDU.ReadHoldingRegisters
+  doctest Modbuzz.PDU.Modbuzz.PDU.ReadInputRegisters
   doctest Modbuzz.PDU.Modbuzz.PDU.WriteSingleCoil
 end

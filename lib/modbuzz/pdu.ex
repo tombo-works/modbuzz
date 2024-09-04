a5eb66cd63b33ce9d7f947a3ea9c82406ce438a6
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-09-05 08:11:27 +0900

    Chore

--- lib/modbuzz/pdu.ex
+++ lib/modbuzz/pdu.ex
@@ -7,15 +7,15 @@ defmodule Modbuzz.PDU do
   def encode_request(struct), do: {:ok, Modbuzz.PDU.Protocol.encode(struct)}
   def encode_response(struct), do: {:ok, Modbuzz.PDU.Protocol.encode(struct)}
 
-  for {modbus_function, modbus_function_code} <- [
-        {ReadCoils, 0x01},
-        {ReadDiscreteInputs, 0x02},
-        {ReadHoldingRegisters, 0x03},
-        {ReadInputRegisters, 0x04},
-        {WriteSingleCoil, 0x05},
-        {WriteSingleRegister, 0x06},
-        {WriteMultipleCoils, 0x0F},
-        {WriteMultipleRegisters, 0x10}
+  for {modbus_function_code, modbus_function} <- [
+        {0x01, ReadCoils},
+        {0x02, ReadDiscreteInputs},
+        {0x03, ReadHoldingRegisters},
+        {0x04, ReadInputRegisters},
+        {0x05, WriteSingleCoil},
+        {0x06, WriteSingleRegister},
+        {0x0F, WriteMultipleCoils},
+        {0x10, WriteMultipleRegisters}
       ] do
     req_module = Module.concat([Modbuzz.PDU, modbus_function, Req])
     res_module = Module.concat([Modbuzz.PDU, modbus_function, Res])

4f2f51d01f6adebfb551b8cfdb26ee03ec595c64
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-09-09 20:17:39 +0900

    Refactor

--- lib/modbuzz/tcp/server/supervisor.ex
+++ lib/modbuzz/tcp/server/supervisor.ex
@@ -6,15 +6,13 @@ defmodule Modbuzz.TCP.Server.Supervisor do
   end
 
   def init(args) do
-    modbuzz_tcp_server_args = Keyword.get(args, :modbuzz_tcp_server_args, [])
-
     children = [
       {Registry, keys: :unique, name: Modbuzz.TCP.Server.Registry},
       {DynamicSupervisor, name: Modbuzz.TCP.Server.DataStoreSupervisor, strategy: :one_for_one},
-      {Modbuzz.TCP.Server.DataStoreOperator, modbuzz_tcp_server_args},
+      {Modbuzz.TCP.Server.DataStoreOperator, args},
       {DynamicSupervisor,
        name: Modbuzz.TCP.Server.SocketHandlerSupervisor, strategy: :one_for_one},
-      {Modbuzz.TCP.Server, modbuzz_tcp_server_args}
+      {Modbuzz.TCP.Server, args}
     ]
 
     Supervisor.init(children, strategy: :one_for_one)

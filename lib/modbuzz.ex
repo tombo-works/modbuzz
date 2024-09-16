dd8852c56cbc4fc0a6c23ac90b283679b6f426f1
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-09-16 14:42:08 +0900

    Remove useless hello/1

--- lib/modbuzz.ex
+++ lib/modbuzz.ex
@@ -2,17 +2,4 @@ defmodule Modbuzz do
   @moduledoc """
   Documentation for `Modbuzz`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Modbuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/modbuzz_test.exs
+++ test/modbuzz_test.exs
@@ -1,8 +1,3 @@
 defmodule ModbuzzTest do
   use ExUnit.Case
-  doctest Modbuzz
-
-  test "greets the world" do
-    assert Modbuzz.hello() == :world
-  end
 end

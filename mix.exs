462074c7ec9972aae730ff172f349f19a046bea0
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-09-23 16:31:32 +0900

    Bump version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Modbuzz.MixProject do
   def project do
     [
       app: :modbuzz,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),

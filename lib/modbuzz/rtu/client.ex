b5bc8269fdfa2e3ca20ce24eb4812bc266df3246
Ryota Kinukawa <kinukawa@tombo-works.com>
2026-05-15 15:36:00 +0900

    Add handle_cast concept to Modbuzz.RTU.Client for async feature

--- lib/modbuzz/rtu/client.ex
+++ lib/modbuzz/rtu/client.ex
@@ -15,6 +15,7 @@ defmodule Modbuzz.RTU.Client do
   end
 
   def init(args) do
+    client_name = Keyword.fetch!(args, :name)
     transport = Keyword.get(args, :transport, Circuits.UART)
     transport_opts = Keyword.get(args, :transport_opts, []) ++ [active: true]
     device_name = Keyword.fetch!(args, :device_name)
@@ -24,6 +25,7 @@ defmodule Modbuzz.RTU.Client do
 
     {:ok,
      %{
+       client_name: client_name,
        transport: transport,
        device_name: device_name,
        transport_pid: transport_pid,
@@ -46,18 +48,48 @@ defmodule Modbuzz.RTU.Client do
       Process.send_after(self(), {:no_response?, adu}, timeout)
       binary = ADU.encode(adu)
 
-      transport.write(transport_pid, binary, timeout)
+      :ok = transport.write(transport_pid, binary, timeout)
 
       {:noreply, %{state | callers: List.replace_at(callers, adu.unit_id, from)}}
     else
-      error = PDU.to_error(request, :server_device_busy)
+      res_tuple = {:error, PDU.to_error(request, :server_device_busy)}
 
-      {:reply, {:error, error}, state}
+      {:reply, res_tuple, state}
+    end
+  end
+
+  def handle_cast({:cast, unit_id, request, pid, timeout}, state) when is_pid(pid) do
+    %{
+      client_name: client_name,
+      transport: transport,
+      transport_pid: transport_pid,
+      callers: callers
+    } = state
+
+    adu = PDU.encode(request) |> ADU.new(unit_id)
+    caller = Enum.fetch!(callers, adu.unit_id)
+
+    if is_nil(caller) do
+      Process.send_after(self(), {:no_response?, adu}, timeout)
+      binary = ADU.encode(adu)
+
+      :ok = transport.write(transport_pid, binary, timeout)
+
+      {:noreply, %{state | callers: List.replace_at(callers, adu.unit_id, pid)}}
+    else
+      res_tuple = {:error, PDU.to_error(request, :server_device_busy)}
+
+      maybe_report_response(pid, client_name, res_tuple)
+
+      {:noreply, state}
     end
   end
 
   def handle_info({:no_response?, adu}, state) do
-    %{callers: callers} = state
+    %{
+      client_name: client_name,
+      callers: callers
+    } = state
 
     caller = Enum.fetch!(callers, adu.unit_id)
 
@@ -68,16 +100,19 @@ defmodule Modbuzz.RTU.Client do
       Log.error("RTU server didn't respond.", nil, state)
       # treat as server device failure
       {:ok, req} = PDU.decode_request(adu.pdu)
-      error = PDU.to_error(req, :server_device_failure)
+      res_tuple = {:error, PDU.to_error(req, :server_device_failure)}
 
-      GenServer.reply(caller, {:error, error})
+      maybe_report_response(caller, client_name, res_tuple)
 
       {:noreply, %{state | callers: List.replace_at(callers, adu.unit_id, nil), binary: <<>>}}
     end
   end
 
   def handle_info({:circuits_uart, _device_name, binary}, state) do
-    %{callers: callers} = state
+    %{
+      client_name: client_name,
+      callers: callers
+    } = state
 
     new_binary = state.binary <> binary
 
@@ -87,9 +122,7 @@ defmodule Modbuzz.RTU.Client do
       res_tuple = PDU.decode_response(pdu)
       caller = Enum.fetch!(callers, unit_id)
 
-      if not is_nil(caller) do
-        GenServer.reply(caller, res_tuple)
-      end
+      maybe_report_response(caller, client_name, res_tuple)
 
       {:noreply, %{state | callers: List.replace_at(callers, unit_id, nil), binary: <<>>}}
     else
@@ -98,13 +131,23 @@ defmodule Modbuzz.RTU.Client do
 
       {:error, %ADU{unit_id: unit_id, pdu: _pdu, crc_valid?: false} = adu} ->
         Log.warning("CRC error detected, #{inspect(adu)}.", nil, state)
+        res_tuple = {:error, :crc_error}
         caller = Enum.fetch!(callers, unit_id)
 
-        if not is_nil(caller) do
-          GenServer.reply(caller, {:error, :crc_error})
-        end
+        maybe_report_response(caller, client_name, res_tuple)
 
         {:noreply, %{state | callers: List.replace_at(callers, unit_id, nil), binary: <<>>}}
+    end
+  end
+
+  defp maybe_report_response(caller, client_name, res_tuple) do
+    cond do
+      # for cast
+      is_pid(caller) -> send(caller, {:modbuzz, client_name, res_tuple})
+      # for call
+      is_tuple(caller) -> GenServer.reply(caller, res_tuple)
+      # for nil
+      true -> :noop
     end
   end
 end
--- test/modbuzz/rtu/client_test.exs
+++ test/modbuzz/rtu/client_test.exs
@@ -41,14 +41,14 @@ defmodule Modbuzz.RTU.ClientTest do
 
     test "return :ok tuple", context do
       Modbuzz.RTU.TransportMock
-      |> expect(:write, fn _pid, _binary, _timeout -> :ok end)
+      |> expect(:write, fn _pid, _binary, _timeout ->
+        send(context.name, {:circuits_uart, "ttyTest", <<0x01, 0x01, 0x00, 0x21, 0x90>>})
+        :ok
+      end)
 
       request = %Modbuzz.PDU.ReadCoils.Req{starting_address: 0, quantity_of_coils: 0}
 
       task = Task.async(fn -> GenServer.call(context.name, {:call, 1, request, 100}) end)
-
-      pid = GenServer.whereis(context.name)
-      Process.send_after(pid, {:circuits_uart, "ttyTest", <<0x01, 0x01, 0x00, 0x21, 0x90>>}, 10)
 
       assert Task.await(task) == {:ok, %Modbuzz.PDU.ReadCoils.Res{byte_count: 0, coil_status: []}}
     end
@@ -65,16 +65,103 @@ defmodule Modbuzz.RTU.ClientTest do
 
     test "return :error tuple, crc error", context do
       Modbuzz.RTU.TransportMock
-      |> expect(:write, fn _pid, _binary, _timeout -> :ok end)
+      |> expect(:write, fn _pid, _binary, _timeout ->
+        send(context.name, {:circuits_uart, "ttyTest", <<0x01, 0x01, 0x00, 0x00, 0x00>>})
+        :ok
+      end)
 
       request = %Modbuzz.PDU.ReadCoils.Req{starting_address: 0, quantity_of_coils: 0}
 
       task = Task.async(fn -> GenServer.call(context.name, {:call, 1, request, 100}) end)
 
-      pid = GenServer.whereis(context.name)
-      Process.send_after(pid, {:circuits_uart, "ttyTest", <<0x01, 0x01, 0x00, 0x00, 0x00>>}, 10)
-
       assert Task.await(task) == {:error, :crc_error}
+    end
+  end
+
+  describe "handle_cast/2" do
+    setup do
+      Modbuzz.RTU.TransportMock
+      |> expect(:start_link, fn [] -> {:ok, self()} end)
+      |> expect(:open, fn _transport_pid, _device_name, _opts -> :ok end)
+
+      name = :client
+
+      _pid =
+        start_link_supervised!(
+          {Modbuzz.RTU.Client,
+           name: name, transport: Modbuzz.RTU.TransportMock, device_name: "ttyTEST"},
+          restart: :temporary
+        )
+
+      %{name: name}
+    end
+
+    test "return :ok tuple", context do
+      Modbuzz.RTU.TransportMock
+      |> expect(:write, fn _pid, _binary, _timeout ->
+        send(context.name, {:circuits_uart, "ttyTest", <<0x01, 0x01, 0x00, 0x21, 0x90>>})
+        :ok
+      end)
+
+      request = %Modbuzz.PDU.ReadCoils.Req{starting_address: 0, quantity_of_coils: 0}
+
+      GenServer.cast(context.name, {:cast, 1, request, self(), 100})
+
+      assert_receive {:modbuzz, :client,
+                      {:ok, %Modbuzz.PDU.ReadCoils.Res{byte_count: 0, coil_status: []}}}
+    end
+
+    test "return :error tuple, no response", context do
+      Modbuzz.RTU.TransportMock
+      |> expect(:write, fn _pid, _binary, _timeout -> :ok end)
+
+      request = %Modbuzz.PDU.ReadCoils.Req{starting_address: 0, quantity_of_coils: 0}
+
+      GenServer.cast(context.name, {:cast, 1, request, self(), 100})
+
+      assert_receive {:modbuzz, :client, {:error, %Modbuzz.PDU.ReadCoils.Err{exception_code: 4}}},
+                     500
+    end
+
+    test "return :error tuple, crc error", context do
+      Modbuzz.RTU.TransportMock
+      |> expect(:write, fn _pid, _binary, _timeout ->
+        send(context.name, {:circuits_uart, "ttyTest", <<0x01, 0x01, 0x00, 0x00, 0x00>>})
+        :ok
+      end)
+
+      request = %Modbuzz.PDU.ReadCoils.Req{starting_address: 0, quantity_of_coils: 0}
+
+      GenServer.cast(context.name, {:cast, 1, request, self(), 100})
+
+      assert_receive {:modbuzz, :client, {:error, :crc_error}}
+    end
+
+    test "return :error tuple, server device busy", context do
+      Modbuzz.RTU.TransportMock
+      |> expect(:write, fn _pid, _binary, _timeout -> :ok end)
+
+      request = %Modbuzz.PDU.ReadCoils.Req{starting_address: 0, quantity_of_coils: 0}
+
+      me = self()
+
+      [pid1, pid2] =
+        for pid_res <- [:pid1_res, :pid2_res] do
+          spawn(fn ->
+            receive do
+              {:modbuzz, :client, res_tuple} -> send(me, {pid_res, res_tuple})
+            end
+          end)
+        end
+
+      # First cast occupies unit_id: 1 with pid1
+      GenServer.cast(context.name, {:cast, 1, request, pid1, 100})
+      # Second cast for the same unit_id should report busy to pid2
+      GenServer.cast(context.name, {:cast, 1, request, pid2, 100})
+
+      # Busy must be reported only to the second requester (pid2), not the first requester (pid1).
+      assert_receive {:pid2_res, {:error, %Modbuzz.PDU.ReadCoils.Err{exception_code: 6}}}
+      refute_receive {:pid1_res, {:error, %Modbuzz.PDU.ReadCoils.Err{exception_code: 6}}}
     end
   end
 end
